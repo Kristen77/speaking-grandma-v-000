b10818db47cbf3952fc15f3fd0b845f2f96e7ee0
Kristen Robertson <kristen77@gmail.com>
2018-09-20 23:10:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.downcase
-    return "Huh?!  Speak up Sonny!".upcase
+    return "HUH?!  SPEAK UP, SONNY!"
   
   elsif "Hi Nana, what did you eat today?".upcase  == "Hi Nana, what did you eat today?".upcase 
     puts "No, not since 1938!".upcase
