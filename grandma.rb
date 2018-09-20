634e244463ad4a8cce5ff89c654c03e4703026ce
Kristen Robertson <kristen77@gmail.com>
2018-09-20 23:30:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase !== phrase.upcase
-    return 
+    return "HUH?! SPEAK UP, SONNY!"
   else 
     "NO, NOT SINCE 1938!"
  end 
